887087f0b5a6d224d8da2aa8d1cb875aa13ff19b
OmerMert <myurdakul65@gmail.com>
2022-04-03 11:20:49 +0300

    Added Basic UI Elements

--- lib/main.dart
+++ lib/main.dart
@@ -11,32 +11,51 @@ class myApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey[50],
+        backgroundColor: Colors.blueGrey[400],
         appBar: AppBar(
-          backgroundColor: Colors.blue,
+          backgroundColor: Colors.green,
           title: Text(
             'Signal Controller',
             style: TextStyle(color: Colors.black, fontSize: 24),
           ),
         ),
-        body: SignalChoose(),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              children: [
+                Expanded(child: SignalType()),
+                Expanded(child: Amplitude()),
+                Expanded(child: Frequency()),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
 }
 
-class SignalChoose extends StatelessWidget {
-  const SignalChoose({Key? key}) : super(key: key);
+class SignalType extends StatelessWidget {
+  const SignalType({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text('Signal Type', style: TextStyle(fontSize: 20 ),),
-        Buttons(),
-        Text('Frequency', style: TextStyle(fontSize: 20 ),),
-        MyStatefulWidget(),
-      ],
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0), color: Colors.white),
+      child: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.all(20.0),
+            child: Text(
+              'Signal Type',
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+          Buttons(),
+        ],
+      ),
     );
   }
 }
@@ -50,23 +69,44 @@ class Buttons extends StatelessWidget {
       children: [
         Expanded(
           child: Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: TextButton(
-                onPressed: () => {}, child: Image.asset('images/sine.png')),
+            padding: const EdgeInsets.all(20.0),
+            child: Container(
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+                  border: Border.all(color: Colors.black)),
+              child: TextButton(
+                  onPressed: () => {}, child: Image.asset('images/sine.png')),
+            ),
           ),
         ),
         Expanded(
           child: Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: TextButton(
-                onPressed: () => {}, child: Image.asset('images/triangle.png')),
+            padding: const EdgeInsets.all(20.0),
+            child: Container(
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+                  border: Border.all(color: Colors.black)),
+              child: TextButton(
+                  onPressed: () => {},
+                  child: Image.asset('images/triangle.png')),
+            ),
           ),
         ),
         Expanded(
           child: Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: TextButton(
-                onPressed: () => {}, child: Image.asset('images/sawtooth.png')),
+            padding: const EdgeInsets.all(20.0),
+            child: Container(
+              height: 75,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+                  border: Border.all(color: Colors.black)),
+              child: TextButton(
+                  onPressed: () => {},
+                  child: Image.asset('images/sawtooth.png')),
+            ),
           ),
         ),
       ],
@@ -74,14 +114,64 @@ class Buttons extends StatelessWidget {
   }
 }
 
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
+class Amplitude extends StatelessWidget {
+  const Amplitude({Key? key}) : super(key: key);
 
   @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      width: 500,
+      height: 150,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0), color: Colors.white),
+      child: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.all(20.0),
+            child: Text(
+              'Amplitude',
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+          FrequencySlider(),
+        ],
+      ),
+    );
+  }
 }
 
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+class Frequency extends StatelessWidget {
+  const Frequency({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0), color: Colors.white),
+      child: Column(children: [
+        Container(
+          margin: EdgeInsets.all(20.0),
+          child: Text(
+            'Frequency',
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+        FrequencySlider(),
+      ]),
+    );
+  }
+}
+
+class FrequencySlider extends StatefulWidget {
+  const FrequencySlider({Key? key}) : super(key: key);
+
+  @override
+  State<FrequencySlider> createState() => _FrequencySliderState();
+}
+
+class _FrequencySliderState extends State<FrequencySlider> {
   double _currentSliderValue = 20;
 
   @override
